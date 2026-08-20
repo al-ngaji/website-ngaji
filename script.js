caba335667e3f0bf5fefddbc3c4dcdb76aa1fbd9
al-ngaji <saudi.achmadluthfifauzan@gmail.com>
2026-08-20 20:40:44 +0700

    Update script.js

---
+++ script.js
@@ -0,0 +1,30 @@
+// ================================
+// MENU MOBILE
+// ================================
+
+function toggleMenu() {
+
+    const menu = document.getElementById("menu");
+
+    menu.classList.toggle("active");
+
+}
+
+
+// ================================
+// TUTUP MENU SETELAH LINK DIKLIK
+// ================================
+
+const menuLinks = document.querySelectorAll("#menu a");
+
+menuLinks.forEach(function(link) {
+
+    link.addEventListener("click", function() {
+
+        const menu = document.getElementById("menu");
+
+        menu.classList.remove("active");
+
+    });
+
+});
